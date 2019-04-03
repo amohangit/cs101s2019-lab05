9c47a01ce223f03ced8a5833680d1502e333645e
Aravind Mohan <amohan@Aravinds-MacBook-Pro-3.local>
2019-04-03 14:09:56 -0400

    first commit

---
+++ src/PartOne.java
@@ -0,0 +1,6 @@
+public class PartOne{
+	/* This is the submission file for the Part One of the lab. */
+	public static void main(String[] args){
+
+	}
+}
---
+++ src/PartTwo.java
@@ -0,0 +1,6 @@
+public class PartTwo{
+	/* This is the submission file for the Part Two of the lab. */
+	public static void main(String[] args){
+
+	}
+}
